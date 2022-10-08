a81c6e371f3615a7254cef6b86f515b906db754f
ClaratZ <113831732+ClaratZ@users.noreply.github.com>
2022-10-08 19:04:26 +0700

    Delete spam.py

--- spam.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-nomor = input("Nomor : ")
-jumlah = int(input("Jumlah : "))
-for i in range(jumlah):
-      url = requests.get(f"https://darkteam.my.id/minispam/spamsms.php?nomor= ")
